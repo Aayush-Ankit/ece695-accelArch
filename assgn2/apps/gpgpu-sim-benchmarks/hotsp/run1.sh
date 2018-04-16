d0c2d9ddec105734bedfa8ffd576958b7a7df8c9
Aayush Ankit <aankit@qstruct.ecn.purdue.edu>
2018-04-16 12:22:57 -0400

    RFC implementation on gpgpu-sim

---
+++ assgn2/apps/gpgpu-sim-benchmarks/hotsp/run1.sh
@@ -0,0 +1,31 @@
+# Run benchmark with different rfc configs
+source ../../../gpgpu-sim/setup_environment debug
+
+## config - RFC with 1 entry per warp
+#printf "Running config 1.....\n\n"
+#cp configs/gpgpusim_rfc1entry.config gpgpusim.config
+#HOTSP 30 6 40 ./result_30_6_40.txt > results/trace_1entry.txt
+#
+## config - RFC with 2 entry per warp
+#printf "Running config 2.....\n\n"
+#cp configs/gpgpusim_rfc2entry.config gpgpusim.config
+#HOTSP 30 6 40 ./result_30_6_40.txt > results/trace_2entry.txt
+
+# config - RFC with 4 entry per warp
+printf "Running config 3.....\n\n"
+cp configs/gpgpusim_rfc4entry.config gpgpusim.config
+HOTSP 30 6 40 ./result_30_6_40.txt > results/trace_4entry.txt
+
+## config - RFC with 6 entry per warp
+#printf "Running config 4.....\n\n"
+#cp configs/gpgpusim_rfc6entry.config gpgpusim.config
+#HOTSP 30 6 40 ./result_30_6_40.txt > results/trace_6entry.txt
+#
+## config - RFC with 8 entry per warp
+#printf "Running config 5.....\n\n"
+#cp configs/gpgpusim_rfc8entry.config gpgpusim.config
+#HOTSP 30 6 40 ./result_30_6_40.txt > results/trace_8entry.txt
+
+# cleanup
+rm _cuobjdump_*
+rm *log
